a15a9de7a6b0843457aa94341aa56891a2961306
Bill Ferster <bferster@virginia.edu>
2019-06-15 12:57:48 -0400

    tree shell

--- doc.js
+++ doc.js
@@ -6,11 +6,11 @@ class Doc {
 
 	constructor(div)																			// CONSTRUCTOR
 	{
-		app.doc=this;
 		this.lights=[];																				// Holds lights
 		this.models=[];																				// Holds models
 		this.Init();																				// Init
-		}
+		this.tree=null;																				// Holds scene tree
+	}
 
 	Init()																						// INIT DOC
 	{
@@ -104,7 +104,7 @@ class Doc {
 		var i;
 		if (!o)	o=this.models;																		// Look in models
 		for (i=0;i<o.length;++i)																	// For each item
-			if (o.id[i] == id)	 return i;															// If a match return index
+			if (o[i].id == id)	 return i;															// If a match return index
 		return -1;																					// Not found
 	}
 	
@@ -130,3 +130,107 @@ class Doc {
 
 }	// DOC CLASS CLOSURE
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// TREE  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Tree {
+
+	constructor()  																	// CONSTRUCTOR
+	{
+		this.Init();																	// Init tree
+	}
+
+	Init(id) 																			// INIT TREE
+	{
+		var i,oo;
+		var _this=this;																	// Save context		
+		var o=app.doc.models[0];														// Point at root												
+		if (!o)	return;																	// If invalid, quit
+		var str="<ul><li class='parent active'><a id='tr-"+o.id+"'>"+o.name+"</a></li>"; // Add root tree node
+		$("#treeDiv").html(str+"</ul>");												// Add to tree div
+		this.AddChildren($("#tr-"+o.id),0);												// Add children to tree
+		for (i=1;i<app.doc.models.length;++i) {											// For each model
+			oo=app.doc.models[i];														// Point at  model
+//			if (!app.doc.FindLobById(oo.parent))										// If no parent
+				str+="<ul><li><a id='tr-"+oo.id+"'>"+oo.name+"</a></li></ul>";			// Add to tree
+			}
+		$("#tr-"+o.id).parent().children('ul').slideToggle('fast');            			// Open course
+		$("[id^=tr-]").draggable( {	revert:true, delay:500 });							// Make lines draggable to change spotin tree
+		$("[id^=tr-]").droppable( {														// Make lines droppable 
+			drop: function(e,ui) {														// On drop
+				var mode=e.shiftKey ? "move" : "shift";									// Move or shift
+				app.Do();																// Save for undo
+//				app.doc.ChangeOrder(ui.draggable[0].id.substr(3),e.target.id.substr(3),mode); // Rearrange
+				_this.Init(ui.draggable[0].id.substr(3));								// Re-init tree	
+				Sound("ding");															// Ding
+				}
+		});
+
+		if (id != undefined) this.Open(id);	
+
+		$('.co-tree li > a').on("click", function(e) {									// ON CLICK OF NODE TEXT
+			_this.handleTreeClick($(this),e);  											// Handle
+			Sound("click"); 															// Click
+			});      
+	}
+
+	Open(id)																		// OPEN TREE AT ID
+	{
+		var i;
+		var row=$("#tr-"+id);															// Row
+		var par=row.parent();															// Point at previous line
+		$('.co-tree li a').each( function() {                          					// For each line
+			$(this).css({"color":"#000","font-weight":"normal"});      					// Normal
+			}); 
+		row.css({"color":"#009900","font-weight":"bold"});          					// Bold and green   
+		for (i=0;i<20;++i) {															// Iterate upwards
+			if ($(par).attr("class") == "co-tree")	break;								// Quit at top of tree
+			if ($(par).attr("class") == "parent") {										// Has children
+				par.addClass('active');                         						// Active class on 
+				par.children('ul').slideToggle('fast');            						// Slide into place
+				}
+			par=par.parent();															// Up a level
+			}
+	}
+
+	handleTreeClick(row, e)																// HANDLE TREE CLICK
+	{
+		if (e.offsetX < 12) {                                         				  		// In icon
+			row.parent().toggleClass('active');                         					// Toggle active class on or off
+			row.parent().children('ul').slideToggle('fast');            					// Slide into place
+			}
+		else{																				// In text
+			$('.co-tree li a').each( function() {                          					// For each line
+				$(this).css({"color":"#000","font-weight":"normal"});      					// Normal
+				}); 
+			row.css({"color":"#009900","font-weight":"bold"});          					// Bold and green   
+			this.curRow=row;																// Save row
+			this.curPaneId=e.target.id.substr(3);											// Save pane id
+			app.curModel=app.doc.FindById(this.curPaneId);									// Set current model
+			app.DrawTopMenu();																// Redraw menu
+			}
+		}
+
+	AddChildren(row, id) 																// ADD CHILDREN TO TREE RECURSIVELY
+	{
+		var i,o,oo;
+		if (id < 0)	return;																	// Invalid index
+		var o=app.doc.models[id];															// Point at parent												
+		if (!o)	return;																		// If invalid, quit
+		if (!o.children)	return;															// Quit if no children
+		var str="<ul style='display:none'>";												// Wrapper
+		for (i=0;i<o.children.length;++i) {													// For each child
+			str+="<li";																		// Start row
+			oo=app.doc.models[o.kids[i]];													// Point at child model via index
+			if (oo.children.length)	str+=" class='parent'"									// Add parent if it has children
+			str+="><a id='tr-"+oo.id+"'>"+oo.name;											// Add index and name
+			str+"</a></li>";																// Add label
+			}
+		row.after(str+"</ul>");																// Add to tree
+		for (i=0;i<o.children.length;++i) {													// For each child
+			row=$("#tr-"+o.children[i])
+			this.AddChildren(row,o.kids[i]);												// Recurse
+			}
+		}
+}																							// Class closure
