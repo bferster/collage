0a0034d7378625c2dc200199edb518be74b04b7a
Bill Ferster <bferster@virginia.edu>
2019-07-05 16:24:20 -0400

    timeline v1.0 done

--- time.js
+++ time.js
@@ -25,7 +25,7 @@ class Time {
 		this.Update(this.curTime);																	// Update timeline
 	}
 
-	Update(time)																				// UPDATE TIMELINE
+	Update(time, dontScroll)																	// UPDATE TIMELINE
 	{
 		var i;
 		if (time != undefined)	this.curTime=time;													// Set current time
@@ -45,15 +45,14 @@ class Time {
 		$("#curTimeBox").val(SecondsToTimecode(this.curTime));										// Update time
 
 		var x=this.TimeToPos(this.curTime);															// Get x pos in timeline
-		x=Math.max(0,$("#timeBarsDiv").width()/2);													// Move to center
-		$("#timeSliderDiv").scrollLeft(x);															// Scroll slider
-		$("#timeScaleDiv").scrollLeft(x);															// Sscale
-		$("#timeBarsDiv").scrollLeft(x);															// Bars
-	}
-
-	TimeToPos(time)																				// CONVERT TIME TO TMELINE POSITION
-	{
-			return time*=$("#timeBarsDiv").width()/10/this.scale;									// Span size for 1 second
+		var x1=$("#timeSliderDiv").scrollLeft();													// Get curreent scrooll pos if not scrolling
+		if (!dontScroll) {																			// If scrolling
+			x1=Math.max(0,x-$("#timeBarsDiv").width()/2);											// Move to center
+			$("#timeSliderDiv").scrollLeft(x1);														// Scroll slider
+			$("#timeScaleDiv").scrollLeft(x1);														// Sscale
+			$("#timeBarsDiv").scrollLeft(x1);														// Bars
+			}
+		$("#timeCursorDiv").css({left:(x+152-x1)+"px"}); 											// Position cursor
 	}
 
 	DrawBars()																					// DRAW TIMELINE BARS
@@ -63,10 +62,10 @@ class Time {
 		var span=$("#timeBarsDiv").width()/10/this.scale;											// Span size
 		var ly=app.doc.scenes[app.curScene].layers;													// Point at layers
 		var dur=app.doc.scenes[app.curScene].style.dur;												// Get duration
-		var w=dur/this.scale*span-2;																// Total width;
-		var str="<div style='float:left;margin-top:6px'>";											// Contain tics
+		var w=dur*span-2;																			// Total width = span * secs
+		str+="<div style='float:left;margin-top:6px;pointer-events:none'>";							// Contain tics
 		var s="<div style='display:inline-block;background-color:#999;width:1px;height:"+h+"px;top:0;margin-left:"+(span-1)+"px'></div>";	// Position info
-		for (i=0;i<dur/this.scale;++i) 	str+=s														// For each time unit, add line
+		for (i=0;i<dur;++i) 	str+=s																// For each second, add line
 		str+="</div><div id='tbar-100' style='width:"+w+"px' class='co-timeBar'></div>";			// Camera bar
 		for (i=0;i<ly.length;++i) {																	// For layer
 			o=app.doc.models[app.doc.FindById(ly[i])];												// Point at layer
@@ -79,25 +78,30 @@ class Time {
 			var x=$("#timeSliderDiv").scrollLeft();													// Get spot in slider
 			$("#timeScaleDiv").scrollLeft(x);														// Scroll scale
 			$("#timeBarsDiv").scrollLeft(x);														// Bars
+			$("#timeCursorDiv").css({left:(this.TimeToPos(this.curTime)+152-x)+"px"}); 				// Position cursor
 			});
-	}
+
+		var y=$("#timeBarsDiv").position().top;														// Cursor top
+		$("#timeCursorDiv").css({top:y+"px",left:"152px",height:(h-26)+"px"}); 						// Position cursor
+		$("#timeBarsDiv").on("click", (e)=> {														// SET TIME
+			this.Update(this.PosToTime(e.offsetX),true);											// Update without scrolling
+			});
+		}
 
 	DrawScale()																					// DRAW TIME SCALE
 	{
-		var i,x=0;																					
+		var i,x=0,str="";																					
 		var h="<span style='position:absolute;top:3px;left:";										// Position info
 		var span=$("#timeBarsDiv").width()/10/this.scale;											// Segment span
 		var dur=app.doc.scenes[app.curScene].style.dur;												// Point at current scene
-		var w=dur/this.scale+span+span+span;														// Go past
-		var str=h+"-50px'>Current time: "+SecondsToTimecode(this.curTime)+"</span>";				// Current time
-		str=""
-		for (i=0;i<w;++i) {																			// For each time unit
-			if (!(i%this.scale)) str+=h+Math.max(0,x-12)+"px'>"+SecondsToTimecode(i*this.scale)+"</span>";		// Set position
+		
+		for (i=0;i<dur;++i) {																		// For each 1-second span
+			if (!(i%this.scale)) str+=h+Math.max(0,x-12)+"px'>"+SecondsToTimecode(i).substr(0,5)+"</span>";	// Set position
 			x+=span;																				// Next spot
 			}
 		$("#timeScaleDiv").html(str);																// Add scale to div
 
-		str="<input id='curTimeBox' class='co-num' type='text' style='font-size:11px;margin:0;width:40px;height:10px'>"	
+		str="<input id='curTimeBox' class='co-num' type='text' style='font-size:11px;margin:0;width:50px;height:10px;color:#666'>"	
 		str+="<img id='contractTime' title='Contract timeline' src='img/collapse.png' style='cursor:pointer;margin-left:25px'>";	
 		str+="<img id='expandTime' title='Expand timeline' src='img/expand.png' style='cursor:pointer;margin-left:25px'>";	
 		$("#timeControlDiv").html(str);																// Add coontrols to div	
@@ -107,7 +111,10 @@ class Time {
 			var now=TimecodeToSeconds(this.value);													// Convert to seconds
 			app.tim.Update(isNaN(now)? undefined : now);											// Update time id a real value
 			});
-	}
+		$("#timeScaleDiv").on("click", (e)=> {														// SET TIME
+			this.Update(this.PosToTime(e.clientX-154+$("#timeBarsDiv").scrollLeft()),true);			// Update without scrolling
+			});
+		}
 
 	DrawLabels()																				// DRAW LABELS
 	{
@@ -128,10 +135,24 @@ class Time {
 			app.SetCurModelById(id);																// Set new model
 			app.sc.TransformController(id);															// Show controller
 			app.topMenuTab=0;																		// Set on layer menu
+			Sound("click");																			// Sound
 			app.DrawTopMenu();																		// Draw menu		
 			});
 
 	}
+
+// HELPERS /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	TimeToPos(time)																				// CONVERT TIME TO TMELINE POSITION
+	{
+		return time*($("#timeBarsDiv").width()/10/this.scale);										// Pos = time / 1-second span width
+	}
+
+	PosToTime(pos)																				// CONVERT  TMELINE POSITION TO TIME
+	{
+		return pos*this.scale/($("#timeBarsDiv").width()/10);										// Time = pos *  
+	}
+
 
 
 }  // TIME CLOSURE
